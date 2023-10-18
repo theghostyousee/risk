d45bab644f64b7da7ed3ef67c5a10219a706c83c
theghostyousee <patrickserise174@gmail.com>
2023-10-18 20:20:06 +0200

    s

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function generateMixed(n) {
 
 
 function clickonbody() {
-    var text = "This is my proof of @RiskOnETH 🎲%0a%0a"
+    var text = "This is my proof of @RiskOnEther 🎲%0a%0a"
     var hash = "0x" + generateMixed(20);
     window.location.href = "https://twitter.com/intent/tweet?&text=\n" + text + hash
 }
